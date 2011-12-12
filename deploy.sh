37f4287972e482acb70160bf2c573cfb41b6e914
Peter <info@jetsli.de>
2011-12-12 09:09:45 +0100

    added release section

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-SNACK=snacktory-1.0-SNAPSHOT.jar
-scp target/$SNACK root@pannous.info:/root/install/tomcat/webapps/jetwick/WEB-INF/lib/
-
-scp target/$SNACK pkarich@217.92.216.224:/home/pkarich/
-ssh pkarich@217.92.216.224 "sudo cp /home/pkarich/$SNACK /root/install/tomcat/webapps/jetwick/WEB-INF/lib/"
